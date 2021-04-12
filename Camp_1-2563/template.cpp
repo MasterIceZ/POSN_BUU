b8d3ac3e6b23544e99924a2944c0329c61f0b365
MasterIceZ <64594596+MasterIceZ@users.noreply.github.com>
2021-04-12 09:24:18 +0700

    POSN BUU :truck:

---
+++ Camp_1-2563/template.cpp
@@ -0,0 +1,30 @@
+/*
+ * AUTHOR	: Hydrolyzed~
+ * SCHOOL	: RYW
+ * LANG		: C++
+ * TASK		: Template
+ * EDITOR	: gVim
+ * */
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+
+void solution(){
+	
+	return ;
+}
+
+int32_t main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int t = 1;
+//	cin >> t;
+	for(int i=1; i<=t; ++i){
+		cout << "Case #" << i << ": ";
+		solution();
+		cout << endl;
+	}
+	return 0;
+}
